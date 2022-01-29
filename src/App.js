8aa6a8fa0f35241fbaebfd28f9475e54a5864570
vrcurrie <vanessa@mangoinnovation.com>
2022-01-28 20:00:05 -0500

    started search engine build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://serene-wing-4e9b5a.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vanessa Currie
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/vrcurrie/dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
